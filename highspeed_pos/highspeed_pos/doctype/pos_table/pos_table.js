f0627a80c715a98fe3783d5fce11f95d6eae5d33
Developer <developer@highspeed.com>
2026-06-07 23:55:39 +0300

    Convert POS Table to standard code-defined DocType

---
+++ highspeed_pos/highspeed_pos/doctype/pos_table/pos_table.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, HIGH SPEED IT and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('POS Table', {
+    // refresh: function(frm) {
+    // }
+});
